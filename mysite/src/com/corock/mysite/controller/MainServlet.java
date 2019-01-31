a6414a59fcd7cf7ef8557d5c3780dd5360f8021f
CoRock <dycorphin@gmail.com>
2019-01-31 17:00:49 +0900

    Implement MySite without page algorithm

---
+++ mysite/src/com/corock/mysite/controller/MainServlet.java
@@ -0,0 +1,40 @@
+package com.corock.mysite.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.corock.mvc.action.AbstractActionFactory;
+import com.corock.mvc.action.Action;
+import com.corock.mvc.action.main.MainActionFactory;
+
+// @WebServlet("")
+public class MainServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void init() throws ServletException {
+		String configPath = getServletConfig().getInitParameter("config");
+		
+		System.out.println("init() called " + configPath);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String actionName = request.getParameter("a");
+		
+		AbstractActionFactory af = new MainActionFactory();
+		Action action = af.getAction(actionName);
+		action.execute(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
